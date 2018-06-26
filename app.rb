a204a044caf77c13bade411af90dea0a560c0b42
Grace Govan <ggovan11@outlook.com>
2018-06-26 14:03:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 class App < Sinatra::Base
 get /info
   # Define your GET '/' route below and respond with "Hello, World!"
-
+ get '/' do 
+    "Welcome to your app!!!! -How do I get it to refresh automatically? "
+  end
 end
